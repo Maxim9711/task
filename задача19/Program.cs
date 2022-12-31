d9f0879ada4cc4603b6d568cea1b5ffb5f01bfe1
Max <Bruce.97@mail.ru>
2022-12-31 09:55:38 +0300

    task 19 and 21

---
+++ задача19/Program.cs
@@ -0,0 +1,28 @@
+﻿bool isTextPalindrome(string text) {
+    string chars = text;
+    int leftSymbol = 0;
+    int rightSymbol = chars.Length - 1;
+    while (leftSymbol < rightSymbol) {
+        if (chars[leftSymbol] != chars[rightSymbol])
+            return false;
+
+        do {
+            leftSymbol++;
+        } while (leftSymbol < rightSymbol && chars[leftSymbol] == ' ');
+
+        do {
+            rightSymbol--;
+        } while (rightSymbol > leftSymbol && chars[rightSymbol] == ' ');
+    }
+    return true;
+}
+
+
+// See https://aka.ms/new-console-template for more information
+Console.Write("Введите пятизначное число: ");
+string num = Console.ReadLine();
+
+if(isTextPalindrome(num))
+    Console.WriteLine($"Число {num} является полиндромом.");
+else
+    Console.WriteLine($"Число {num} не является полиндромом.");
---
+++ задача21/Program.cs
@@ -0,0 +1,31 @@
+﻿// x, y, z
+// crd - coordinates
+double dist(int [] crdFirst, int [] crdSecond){
+    double dist = 0;
+    dist = Math.Sqrt((crdSecond[0] - crdFirst[0])*(crdSecond[0] - crdFirst[0]) + 
+                     (crdSecond[1] - crdFirst[1])*(crdSecond[1] - crdFirst[1]) +
+                     (crdSecond[2] - crdFirst[2])*(crdSecond[2] - crdFirst[2])); 
+    return dist;
+}
+
+int[] pointFirst = new int[] {0, 0, 0};
+int[] pointSecond = new int[] {0, 0, 0};
+
+Console.WriteLine($"Введите координаты первой точки");
+Console.Write($"x: ");
+pointFirst[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write($"y: ");
+pointFirst[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write($"z: ");
+pointFirst[2] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите координаты второй точки");
+Console.Write($"x: ");
+pointSecond[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write($"y: ");
+pointSecond[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write($"z: ");
+pointSecond[2] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Расстояние между точками: ");
+Console.WriteLine(dist(pointFirst, pointSecond));
