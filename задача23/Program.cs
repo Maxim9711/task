6c1689a4c2d48fb763aa502f902e5003afa3cfc8
Max <Bruce.97@mail.ru>
2022-12-31 10:02:33 +0300

    task 23

---
+++ задача23/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Кубы чисел до {num}");
+for (int i = 1; i <= num; i++)
+    Console.Write($"{i*i*i} ");
